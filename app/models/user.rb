f39618f436a670c02fe26818538e858866cee740
babiracka <alina.babiracka@gmail.com>
2015-11-29 23:12:56 +0100

    Added devise to User model, fixed routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
+    devise :database_authenticatable, :registerable,
+       :recoverable, :rememberable, :trackable, :validatable
     attr_accessor :email, :password
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  root to: "home#index"
+  
+  devise_for :users
   resources :students do
+    get :subjects
+  end
+  resources :teachers do
     get :subjects
   end
 end
