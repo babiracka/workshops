3c84be5ad772d6b38877c77947f5298c2b15b8c9
babiracka <alina.babiracka@gmail.com>
2015-12-06 23:22:41 +0100

    Added spec for birthdate decorator

--- spec/decorators/student_decorator_spec.rb
+++ spec/decorators/student_decorator_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 describe StudentDecorator do
   let(:teacher) { create :teacher, first_name: 'John', last_name: 'Smith' }
-  let(:student) { create :student, first_name: 'John', last_name: 'Smith' }
+  let(:student) { create :student, first_name: 'John', last_name: 'Smith', birthdate: nil }
+  let(:student_2) { create :student, first_name: 'John', last_name: 'Smith', birthdate: '1992-03-31' }
   let(:subject_item) { create :subject_item, teacher: teacher }
   let(:second_subject_item) { create :subject_item }
   let!(:note_1) { create :subject_item_note, value: 5, student: student, subject_item: second_subject_item }
@@ -28,6 +29,19 @@ describe StudentDecorator do
       it 'calculates avg of student notes' do
         is_expected.to eq '4.50'
       end
+    end
+  end
+
+  describe "#underscored_birthdate" do
+    describe "when student doesn't have birthdate assigned" do
+    subject { student.decorate.underscored_birthdate }
+
+    it { is_expected.to eq "n/a" }
+    end
+
+    describe 'when student has birthdate assigned' do
+    subject { student_2.decorate.underscored_birthdate }
+    it { is_expected.to eq '1992-03_31' }
     end
   end
 end
